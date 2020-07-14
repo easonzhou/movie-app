f4aec9d599dbc3d6b10eeb8e5dd181a60079fbe3
Eason Zhou <yinsheng@sph.com.sg>
2020-07-14 20:28:40 +0800

    Updates

---
+++ precache-manifest.09d56f93a0f647afa0b80234cb4afcec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93c8bec786204fbe1d0a66097c945af",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "470714ed0a2a05a267b3",
+    "url": "/movie-app/static/css/main.327662d8.chunk.css"
+  },
+  {
+    "revision": "b88c2b6293afcb91ba2c",
+    "url": "/movie-app/static/js/2.f5578a36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.f5578a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470714ed0a2a05a267b3",
+    "url": "/movie-app/static/js/main.9877d893.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
